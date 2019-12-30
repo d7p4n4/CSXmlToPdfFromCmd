0c8721d9731af43624faa07c718d610fefa45d5f
Zsombor Bérces <32767685+d7p4n4@users.noreply.github.com>
2019-12-30 16:14:19 +0100

    1.20191230.2

--- CSXmlToPdfFromCmd/Program.cs
+++ CSXmlToPdfFromCmd/Program.cs
@@ -13,27 +13,12 @@ namespace CSXmlToPdfFromCmd
         {
             string time = string.Format("{0:HH:mm:ss tt}", DateTime.Now);
 
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i < 100; i++)
             {
                 try
                 {
                     OpenCommandLine("" + i);
-
-                    //System.Diagnostics.Process.Start(@"d:\transform.bat");
-                    /*
-                    Process cmd = new Process();
-                    cmd.StartInfo.FileName = "cmd.exe";
-                    cmd.StartInfo.RedirectStandardInput = true;
-                    cmd.StartInfo.RedirectStandardOutput = true;
-                    cmd.StartInfo.CreateNoWindow = true;
-                    cmd.StartInfo.UseShellExecute = false;
-                    cmd.Start();
-
-                    cmd.StandardInput.WriteLine("echo Oscar");
-                    cmd.StandardInput.Flush();
-                    cmd.StandardInput.Close();
-                    cmd.WaitForExit();
-                    Console.WriteLine(cmd.StandardOutput.ReadToEnd());*/
+                    
                 }
                 catch (Exception exception)
                 {
@@ -45,32 +30,7 @@ namespace CSXmlToPdfFromCmd
             Console.WriteLine("Start: " + time);
             Console.WriteLine("End: " + time2);
         }
-
-        static void ExecuteCommand(string command)
-        {
-            var processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
-            processInfo.CreateNoWindow = true;
-            processInfo.UseShellExecute = false;
-            processInfo.RedirectStandardError = true;
-            processInfo.RedirectStandardOutput = true;
-
-            var process = Process.Start(processInfo);
-
-            process.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
-                Console.WriteLine("output>>" + e.Data);
-            process.BeginOutputReadLine();
-
-            process.ErrorDataReceived += (object sender, DataReceivedEventArgs e) =>
-                Console.WriteLine("error>>" + e.Data);
-            process.BeginErrorReadLine();
-
-            process.WaitForExit();
-
-            Console.WriteLine("ExitCode: {0}", process.ExitCode);
-            process.Close();
-        }
-
-        private static void OpenCommandLine(string output)
+                private static void OpenCommandLine(string output)
         {
             try
             {
@@ -88,37 +48,6 @@ namespace CSXmlToPdfFromCmd
 
             }
         }
-
-        public void ExecuteCommandSync(object command)
-        {
-            try
-            {
-                // create the ProcessStartInfo using "cmd" as the program to be run,
-                // and "/c " as the parameters.
-                // Incidentally, /c tells cmd that we want it to execute the command that follows,
-                // and then exit.
-                System.Diagnostics.ProcessStartInfo procStartInfo =
-                    new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
-
-                // The following commands are needed to redirect the standard output.
-                // This means that it will be redirected to the Process.StandardOutput StreamReader.
-                procStartInfo.RedirectStandardOutput = true;
-                procStartInfo.UseShellExecute = false;
-                // Do not create the black window.
-                procStartInfo.CreateNoWindow = true;
-                // Now we create a process, assign its ProcessStartInfo and start it
-                System.Diagnostics.Process proc = new System.Diagnostics.Process();
-                proc.StartInfo = procStartInfo;
-                proc.Start();
-                // Get the output into a string
-                string result = proc.StandardOutput.ReadToEnd();
-                // Display the command output.
-                Console.WriteLine(result);
-            }
-            catch (Exception objException)
-            {
-                // Log the exception
-            }
-        }
+        
     }
 }
