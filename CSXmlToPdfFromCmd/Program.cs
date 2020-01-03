dfc00b19e613de43aa1646b9105844e0c5ea4483
Zsombor Bérces <32767685+d7p4n4@users.noreply.github.com>
2020-01-03 21:00:14 +0100

    1.20200103.1

--- CSXmlToPdfFromCmd/Program.cs
+++ CSXmlToPdfFromCmd/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.IO;
 
 namespace CSXmlToPdfFromCmd
 {
@@ -17,44 +18,43 @@ namespace CSXmlToPdfFromCmd
             //Create pdf document
 
             string time = string.Format("{0:HH:mm:ss tt}", DateTime.Now);
+            string batchFile = "@echo off \ncd d:\\fop-1.0\\ \n";
 
-            for (int i = 0; i < 1; i++)
-            {
-                try
+            try
+            {/*
+                for (int i = 0; i < 1000; i++)
                 {
-                    Aspose.Pdf.Document pdf = new Aspose.Pdf.Document();
-                    //Bind XML and XSLT files to the document
-                    try
-                    {
-                        pdf.BindXml("c:\\evesAlakulas1.xml", "c:\\evesAlakulas1.xslt");
-                    }
-                    catch (System.Exception)
-                    {
+                    batchFile = batchFile + "CALL fop.bat -c d:\\fop-1.0\\conf\\fop.xconf -xml d:\\Server\\bizalmas\\OXAKPDF\\XMLs\\egyeni.xml -xsl " +
+                        "d:\\Server\\bizalmas\\OXAKPDF\\XSLTs\\EgeszDokumentum2.xsl -pdf d:\\Server\\Visual_studio\\AK\\PDFs\\out" + i + ".pdf \n\n";
 
-                        throw;
-                    }
+                }
 
-                    //Save the document
-                    pdf.Save("d:\\HelloWorldUsingXmlAndXslt.pdf");
-                    //OpenCommandLine("" + i);
-                    
-                }
-                catch (Exception exception)
-                {
-                    Console.WriteLine(exception.Message);
-                }
+                writeOut(batchFile, "d:\\Server\\bizalmas\\OXAKPDF\\");*/
+                OpenCommandLine();
             }
+            catch(Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
             string time2 = string.Format("{0:HH:mm:ss tt}", DateTime.Now);
 
             Console.WriteLine("Start: " + time);
             Console.WriteLine("End: " + time2);
         }
-                private static void OpenCommandLine(string output)
+
+        public static void writeOut(string text, string outputPath)
+        {
+            File.WriteAllText(outputPath + "transform.bat", text);
+
+        }
+
+        private static void OpenCommandLine()
         {
             try
             {
-                string MyBatchFile = @"D:\transform.bat";
-                Process process = new Process { StartInfo = { Arguments = string.Format("{0}", output) } };
+                string MyBatchFile = @"D:\Server\bizalmas\OXAKPDF\transform.bat";
+                Process process = new Process();
                 process.StartInfo.FileName = MyBatchFile;
                 process.Start();
             }
